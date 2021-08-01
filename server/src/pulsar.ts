c689093bcf603dd7d52d90dfbc7e3b36bdeb3bb7
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2021-08-01 18:00:20 +0200

    Convert tabs to 4 spaces

--- server/src/pulsar.ts
+++ server/src/pulsar.ts
@@ -11,98 +11,98 @@ const pulsarScript = slash(path.normalize(path.join(__dirname, '..', 'res', 'run
 const pulsarModule = slash(path.normalize(path.join(__dirname, '..', 'extern', 'pulsar')));
 
 export class Pulsar {
-	public analyze(sourceFile: TextDocument, settings: JStarSettings): Promise<Diagnostic[]> {
-		return new Promise<Diagnostic[]>((resolve, reject) => {
-			let pulsarProc = spawn(settings.jstarExecutable, [
-				'-E', '-e', `importPaths.insert(0, "${pulsarModule}")`,
-				pulsarScript, sourceFile.uri, sourceFile.getText()
-			].concat(this.buildOptionList(settings)));
+    public analyze(sourceFile: TextDocument, settings: JStarSettings): Promise<Diagnostic[]> {
+        return new Promise<Diagnostic[]>((resolve, reject) => {
+            let pulsarProc = spawn(settings.jstarExecutable, [
+                '-E', '-e', `importPaths.insert(0, "${pulsarModule}")`,
+                pulsarScript, sourceFile.uri, sourceFile.getText()
+            ].concat(this.buildOptionList(settings)));
 
-			let stdoutBuf: string[] = [];
-			let stderrBuf: string[] = [];
+            let stdoutBuf: string[] = [];
+            let stderrBuf: string[] = [];
 
-			pulsarProc.stdout.on('data', (data: string) => {
-				stdoutBuf.push(data);
-			})
+            pulsarProc.stdout.on('data', (data: string) => {
+                stdoutBuf.push(data);
+            })
 
-			pulsarProc.stderr.on('data', (data: string) => {
-				stderrBuf.push(data);
-			})
+            pulsarProc.stderr.on('data', (data: string) => {
+                stderrBuf.push(data);
+            })
 
-			pulsarProc.on('error', (err: Error) => {
-				reject(new PulsarExecutionError(`Cannot find J* executable: ${err.message}`));
-			})
+            pulsarProc.on('error', (err: Error) => {
+                reject(new PulsarExecutionError(`Cannot find J* executable: ${err.message}`));
+            })
 
-			pulsarProc.on('close', (code: integer) => {
-				if (code != 0) {
-					reject(new PulsarExecutionError(`Error executing pulsar: ${stderrBuf.join().trim()}`));
-					return;
-				}
+            pulsarProc.on('close', (code: integer) => {
+                if (code != 0) {
+                    reject(new PulsarExecutionError(`Error executing pulsar: ${stderrBuf.join().trim()}`));
+                    return;
+                }
 
-				const diagnostics: Diagnostic[] = [];
-				let outputString = stdoutBuf.join().trim();
-				let outputLines = outputString ? outputString.split("\n") : [];
+                const diagnostics: Diagnostic[] = [];
+                let outputString = stdoutBuf.join().trim();
+                let outputLines = outputString ? outputString.split("\n") : [];
 
-				for (const { index, element: line } of enumerate(outputLines)) {
-					if (index == settings.maxNumberOfProblems - 1) break;
+                for (const { index, element: line } of enumerate(outputLines)) {
+                    if (index == settings.maxNumberOfProblems - 1) break;
 
-					let jsonDiagnostic = JSON.parse(line)
+                    let jsonDiagnostic = JSON.parse(line)
 
-					const diagnostic: Diagnostic = {
-						severity: jsonDiagnostic.severity == 'error' ?
-							DiagnosticSeverity.Error : DiagnosticSeverity.Warning,
-						range: {
-							start: sourceFile.positionAt(jsonDiagnostic.start),
-							end: sourceFile.positionAt(jsonDiagnostic.end)
-						},
-						message: jsonDiagnostic.message,
-						source: 'pulsar'
-					};
+                    const diagnostic: Diagnostic = {
+                        severity: jsonDiagnostic.severity == 'error' ?
+                            DiagnosticSeverity.Error : DiagnosticSeverity.Warning,
+                        range: {
+                            start: sourceFile.positionAt(jsonDiagnostic.start),
+                            end: sourceFile.positionAt(jsonDiagnostic.end)
+                        },
+                        message: jsonDiagnostic.message,
+                        source: 'pulsar'
+                    };
 
-					diagnostics.push(diagnostic);
-				}
+                    diagnostics.push(diagnostic);
+                }
 
-				resolve(diagnostics);
-			});
-		});
-	}
+                resolve(diagnostics);
+            });
+        });
+    }
 
-	private buildOptionList(settings: JStarSettings): Array<string> {
-		let optionList: Array<string> = [];
-		if (settings.disableVarResolve)
-			optionList.push('-v');
-		if (settings.noRedefinedGlobals)
-			optionList.push('-g');
-		if (settings.disableUnreachPass)
-			optionList.push('-U');
-		if (settings.noUnusedArgs)
-			optionList.push('-a');
-		if (settings.disableUnusedPass)
-			optionList.push('-u');
-		if (settings.disableReturnPass)
-			optionList.push('-r');
-		if (settings.disableAccessPass)
-			optionList.push('-A');
-		return optionList;
-	}
+    private buildOptionList(settings: JStarSettings): Array<string> {
+        let optionList: Array<string> = [];
+        if (settings.disableVarResolve)
+            optionList.push('-v');
+        if (settings.noRedefinedGlobals)
+            optionList.push('-g');
+        if (settings.disableUnreachPass)
+            optionList.push('-U');
+        if (settings.noUnusedArgs)
+            optionList.push('-a');
+        if (settings.disableUnusedPass)
+            optionList.push('-u');
+        if (settings.disableReturnPass)
+            optionList.push('-r');
+        if (settings.disableAccessPass)
+            optionList.push('-A');
+        return optionList;
+    }
 }
 
 export class PulsarExecutionError extends Error {
-	constructor(message: string) {
-		super(message);
-	}
+    constructor(message: string) {
+        super(message);
+    }
 }
 
 // An element of an Iterable along with its index
 interface EnumPair<T> {
-	index: number;
-	element: T;
+    index: number;
+    element: T;
 }
 
 // Util function that returns the elements of an iterable along with their index.
 // The function is a generator in order to compute the result lazily.
 function* enumerate<T>(iterable: Iterable<T>, start: number = 0): Generator<EnumPair<T>> {
-	for (let element of iterable) {
-		yield { index: start++, element: element };
-	}
+    for (let element of iterable) {
+        yield { index: start++, element: element };
+    }
 }
--- server/src/settings.ts
+++ server/src/settings.ts
@@ -1,11 +1,11 @@
 export interface JStarSettings {
-	jstarExecutable: string;
-	maxNumberOfProblems: number;
-	disableVarResolve: boolean;
-	noRedefinedGlobals: boolean;
-	disableUnreachPass: boolean;
-	noUnusedArgs: boolean;
-	disableUnusedPass: boolean;
-	disableReturnPass: boolean;
-	disableAccessPass: boolean;
+    jstarExecutable: string;
+    maxNumberOfProblems: number;
+    disableVarResolve: boolean;
+    noRedefinedGlobals: boolean;
+    disableUnreachPass: boolean;
+    noUnusedArgs: boolean;
+    disableUnusedPass: boolean;
+    disableReturnPass: boolean;
+    disableAccessPass: boolean;
 }
